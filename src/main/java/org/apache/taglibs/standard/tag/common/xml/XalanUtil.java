094f694008d186d13ae1228f5e6f78e21e20c2ca
Tomaz Cerar <tomaz.cerar@gmail.com>
2014-05-07 15:51:13 +0200

    Remove unused imports

--- src/main/java/org/apache/taglibs/standard/tag/common/xml/XalanUtil.java
+++ src/main/java/org/apache/taglibs/standard/tag/common/xml/XalanUtil.java
@@ -22,9 +22,7 @@ import javax.servlet.jsp.tagext.TagSupport;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 
 import org.apache.xpath.VariableStack;
 import org.apache.xpath.XPathContext;
